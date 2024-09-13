c992944b87dfa7888746edb0d3970e5208b4536b
chiamalite-codingal <chiamalitecodingal@gmail.com>
2024-09-13 18:18:30 +0100

    conditional statement

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ number = -2
 if (number>1):
     print("Number is greater than 1")
 
-    number =- 8
-    if (number>1):
-        print("Number is greater than 1")
+number =- 8
+if (number>1):
+     print("Number is greater than 1")
